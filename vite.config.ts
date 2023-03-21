02df740c6119c3ed4ae980d203e867c57dece0c1
duncsully <duncsully@gmail.com>
2023-03-20 19:24:15 -0500

    feat: attempt making into PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      workbox: { globPatterns: ['**/*.{js,css,html,svg}'] },
+      includeAssets: ['public/defaultOtto.svg'],
+      manifest: {
+        name: 'OttaDo',
+        short_name: 'OttaDo',
+        description: 'OttaDo is a productivity app that works offline!',
+        theme_color: '#121212',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
