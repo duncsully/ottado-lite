fe2faf0c61d9bdf45e4eb921f19ab71c5ff68fb6
duncsully <duncsully@gmail.com>
2023-03-20 20:04:50 -0500

    fix: set base for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Deployed on GH pages so we need to set the base to the repo name
+  base: 'ottado-lite',
   plugins: [
     react(),
     VitePWA({
