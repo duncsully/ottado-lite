6ad1fa17a1b7aea46e6a05eb1207ba0f4b87d352
duncsully <duncsully@gmail.com>
2023-03-20 20:43:54 -0500

    fix: another icon fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -20,17 +20,17 @@ export default defineConfig({
         theme_color: '#121212',
         icons: [
           {
-            src: 'public/android-chrome-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'public/android-chrome-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'public/android-chrome-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
