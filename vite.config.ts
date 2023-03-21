d043d70a1e75f00970a64042f98112da609b9c58
duncsully <duncsully@gmail.com>
2023-03-20 20:08:48 -0500

    fix: set base correctly dur

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Deployed on GH pages so we need to set the base to the repo name
-  base: 'ottado-lite',
+  base: '/ottado-lite/',
   plugins: [
     react(),
     VitePWA({
