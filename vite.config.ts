1de8ea422defd677db1e9b6a909953aeaa7e08f6
duncsully <duncsully@gmail.com>
2023-04-19 19:00:59 -0500

    fix: attempt getting out of state of being unable to update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       devOptions: { enabled: true },
       workbox: { globPatterns: ['**/*.{js,css,html,png,svg}'] },
       includeAssets: ['public/defaultOtto.svg'],
